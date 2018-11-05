fb878070308e529d5e9bb5458860098100401cd3
marco <marco@DESKTOP-0OSAOAK.local>
2018-11-05 20:33:44 -0300

    update interface grafica

--- app/views/admin/usuario/listar.php
+++ app/views/admin/usuario/listar.php
@@ -15,25 +15,63 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <?php foreach ($viewData["usuarios"] as $usuario) { ?>
+                    <?php foreach ($viewData["usuarios"] as $equipamento) { ?>
                         <tr class="cor1">
-                            <td><?php echo $usuario->id_usuario ?></td>
-                            <td><?php echo $usuario->nome . " " . $usuario->sobrenome ?></td>
-                            <td><?php echo $usuario->email ?></td>
-                            <td><?php echo $usuario->login ?></td>
+                            <td><?php echo $equipamento->id_usuario ?></td>
+                            <td><?php echo $equipamento->nome . " " . $equipamento->sobrenome ?></td>
+                            <td><?php echo $equipamento->email ?></td>
+                            <td><?php echo $equipamento->login ?></td>
 
-  
-                            
-                            <td align="center">
-    
-                                <a href="<?php echo URL_BASE . "usuario/edite/" . $usuario->id_usuario ?>"  class="btn btn-primary mr-3">Editar</a>
-                                <a href="<?php echo URL_BASE . "cliente/delete/" . $usuario->id_cliente ?>" class="btn btn-danger">Excluir</a>
-                            </td>
+
+                            <td class="text-center">
+                                <button type="button" href="<?php echo URL_BASE . "cliente/edite/" . $equipamento->id_cliente ?>"  class="btn btn-primary mr-4" data-toggle="modal" data-target="#modalEditar">Editar</button>
+                                <button type="button" href="<?php echo URL_BASE . "cliente/delete/" . $equipamento->id_cliente ?>" class="btn btn-danger" data-toggle="modal" data-target="#modalExcluir">Excluir</button>
+                            </td>   
 
                         </tr>	
                     <?php } ?>		
                 </tbody>
             </table>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="modalEditar" tabindex="-1" role="dialog" aria-labelledby="modalAlterar" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Deseja realmente editar ?</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-info" data-dismiss="modal">Editar</button>
+                <button type="button" class="btn btn-secondary">Cancelar</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="modalExcluir" tabindex="-1" role="dialog" aria-labelledby="modalAlterar" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Deseja realmente excluir ?</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-info" data-dismiss="modal">Excluir</button>
+                <button type="button" class="btn btn-secondary">Cancelar</button>
+            </div>
         </div>
     </div>
 </div>
--- app/views/painel.php
+++ app/views/painel.php
@@ -9,6 +9,7 @@
         <link href=" <?php echo URL_BASE . "assets/Sass/node_modules/bootstrap/compiler/bootstrap.css" ?>" rel="stylesheet">
         <link href=" <?php echo URL_BASE . "assets/vendor/fontawesome-free/css/all.min.css" ?>" rel="stylesheet" type="text/css">
         <link href=" <?php echo URL_BASE . "assets/_css/sb-admin.css" ?>" rel="stylesheet">  
+        <link href=" <?php echo URL_BASE . "assets/vendor/datatables/dataTables.bootstrap4.css" ?>" rel="stylesheet">
 
     
     </head>
@@ -30,6 +31,9 @@
         <script src=" <?php echo URL_BASE . "assets/vendor/jquery-easing/jquery.easing.min.js"?>"></script>
         <script src=" <?php echo URL_BASE . "assets/_js/sb-admin.min.js" ?>"</script>
         <script src=" <?php echo URL_BASE . "assets/Sass/node_modeules/popper/dist/popper.min.js" ?>"</script>
+        <script src=" <?php echo URL_BASE . "assets/_js/demo/datatable-demo.js" ?>"</script>
+        <script src=" <?php echo URL_BASE . "assets/vendor/datatables/jquery.dataTables.js"?>"></script> 
+        <script src=" <?php echo URL_BASE . "assets/vendor/datatables/dataTables.bootstrap2.css"?>"></script> 
          
     </body>
-</html>
+</html
