30e12d42c13c78e87f6c50a6a1a68717604262ca
marco <marco@DESKTOP-0OSAOAK.local>
2018-11-06 15:36:53 -0300

    update usuario

---
+++ app/views/usuario/cadastro_chamado.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marco
+ * Date: 06/11/2018
+ * Time: 13:22
+ */
+?>
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-sm-12 col-md-10 col-lg-8">
+            <form action="<?php echo URL_BASE . "" ?>" method="POST">
+                <div  class="form-row">
+                    <div class="form-group col-sm-6">
+                        <label for="login">Local</label>
+                        <input type="text" class="form-control" name="login" placeholder="Local">
+                    </div>
+                    <div class="form-group col-sm-6">
+                        <label for="problema">Prioridade</label>
+                        <select class="form-control form-control-md" name="problema">
+                            <option>Selecione</option>
+                        </select>
+                    </div>
+                    <div class="form-group col-sm-6">
+                        <label for="problema">Área</label>
+                        <select class="form-control form-control-md" name="problema">
+                            <option>Selecione</option>
+                        </select>
+                    </div>
+                    <div class="form-group col-sm-3">
+                        <fieldset disabled>
+                            <label for="dataAbertura">Data de abertura</label>
+                            <input type="text" class="form-control" name="dataAbertura">
+                        </fieldset>
+                    </div>
+                    <div class="form-group col-sm-3">
+                        <fieldset disabled>
+                            <label for="dataAtendimento">Data de atendimento </label>
+                            <input type="text" class="form-control" name="dataAtendimento">
+                        </fieldset>
+                    </div>
+                    <div class="form-group col-sm-10">
+                        <label for="descProblema">Descrição problema</label>
+                        <textarea class="form-control" id="descProblema" rows="5"></textarea>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
