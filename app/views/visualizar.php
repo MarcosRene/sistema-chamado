6348f1a9c2c407865d8d032cbbca7920ce1a5ec1
marco <renemoura789@gmail.com>
2018-11-15 09:16:34 -0300

    updete interface grafica

---
+++ app/views/visualizar.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marco
+ * Date: 15/11/2018
+ * Time: 09:14
+ */
+?>
+<div class="container">
+    <div class="row justify-content-center">
+
+        <div class="col-sm-12 col-md-10 col-lg-10">
+
+            <form action="" method="POST">
+
+                <div class="row">
+
+                    <div class="form-row col-lg-7" id=form_row>
+
+                        <div class="form-group col-sm-4">
+                            <fieldset disabled>
+                                <label for="codigo">Código</label>
+                                <input class="form-control" type="text" name="codigo" placeholder="teste">
+                            </fieldset>
+                        </div>
+
+                        <div class="form-group col-sm-4">
+                            <fieldset disabled>
+                                <label for="nome">Área</label>
+                                <input type="text" class="form-control" name="area" placeholder="teste">
+                            </fieldset>
+                        </div>
+
+
+                        <div class="form-group col-sm-4">
+                            <fieldset disabled>
+                                <label for="prioridade">Prioridade</label>
+                                <input type="text" class="form-control" name="prioridade" placeholder="teste">
+                            </fieldset>
+                        </div>
+
+                        <div class="form-group col-sm-6">
+                            <fieldset disabled>
+                                <label for="tombamento">Tombamento</label>
+                                <input type="text" class="form-control" name="tombamento" placeholder="teste">
+                            </fieldset>
+                        </div>
+
+                        <div class="form-group col-sm-6">
+                            <fieldset disabled>
+                                <label for="resposavel">Aberto por</label>
+                                <input type="text" class="form-control" name="responsavel" placeholder="teste">
+                            </fieldset>
+                        </div>
+
+                        <div class="form-group col-sm-6">
+                            <fieldset disabled>
+                                <label for="data-abertura">Data Abertura</label>
+                                <input type="text" class="form-control" name="data-abertura" placeholder="teste">
+                            </fieldset>
+                        </div>
+
+
+                        <div class="form-group col-sm-6">
+                            <fieldset disabled>
+                                <label for="data-abertura">Data Encerramento</label>
+                                <input type="text" class="form-control" name="data-abertura" placeholder="teste">
+                            </fieldset>
+                        </div>
+
+                        <div class="form-group col-sm-12">
+                            <fieldset disabled>
+                                <label for="descricao-problema">Descrição problema</label>
+                                <textarea class="form-control" id="descricao-problema" rows="5"></textarea>
+                            </fieldset>
+                        </div>
+
+                    </div>
+
+                    <div class="form-row col-lg-4" id=form_row>
+                        <div class="form-group col-sm-12">
+                            <fieldset disabled>
+                                <label for="resposavel">Status</label>
+                                <input type="text" class="form-control" name="responsavel" placeholder="teste">
+                            </fieldset>
+                        </div>
+
+                        <div class="form-group col-sm-12">
+                            <fieldset disabled>
+                                <label for="data-abertura">Data Atendimento</label>
+                                <input type="text" class="form-control" name="data-abertura" placeholder="teste">
+                            </fieldset>
+                        </div>
+
+
+                        <div class="form-group col-sm-12">
+                            <fieldset disabled>
+                                <label for="resposavel">Responsável</label>
+                                <input type="text" class="form-control" name="responsavel" placeholder="teste">
+                            </fieldset>
+                        </div>
+
+                        <div class="form-group col-sm-12">
+                            <fieldset disabled>
+                                <label for="parecer">Parecer</label>
+                                <textarea class="form-control" id="parecer" rows="5"></textarea>
+                            </fieldset>
+                        </div>
+
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
