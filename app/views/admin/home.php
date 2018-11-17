32a5eeb656be6d1c9181a0158680652ef65d5eef
marco <renemoura789@gmail.com>
2018-11-17 17:07:22 -0300

    ajustes

--- app/views/admin/home.php
+++ app/views/admin/home.php
@@ -1,15 +1,19 @@
 <div class="row">
-    <div class="col-lg-3 col-md-6 col-sm-6 mb-3">
-        <div class="card text-white bg-danger o-hidden h-100">
-            <div class="card-body">
-                <div class="mr-5">Não atendidos</div>
+    <a class="a">
+        <div class="col-lg-3 col-md-6 col-sm-6 mb-3">
+            <div class="card text-white bg-danger o-hidden h-100">
+                <div class="card-body">
+                    <div class="mr-5">Não atendidos</div>
+                </div>
+                <a class="card-footer text-white text-right p-auto">
+                    <h3><?php echo count($viewData['naoatendidos']) ?></h3>
+                </a>
             </div>
-            <a class="card-footer text-white text-right p-auto">
-                <h3><?php echo count($viewData['naoatendidos']) ?></h3>
-            </a>
         </div>
-    </div>
-    <div class="col-lg-3 col-md-6 col-sm-6 mb-3">
+    </a>
+</div>
+<div class="col-lg-3 col-md-6 col-sm-6 mb-3">
+    <a class="a">
         <div class="card text-white bg-success o-hidden h-100">
             <div class="card-body">
                 <div class="mr-5">Em atendimento</div>
@@ -18,8 +22,10 @@
                 <h3><?php echo count($viewData['ematendimento']) ?></h3>
             </a>
         </div>
-    </div>
-    <div class="col-lg-3 col-md-6 col-sm-6 mb-3">
+    </a>
+</div>
+<div class="col-lg-3 col-md-6 col-sm-6 mb-3">
+    <a class="a">
         <div class="card text-white bg-warning o-hidden h-100">
             <div class="card-body">
                 <div class="mr-5">Aguardando terceiros</div>
@@ -28,8 +34,10 @@
                 <h3><?php echo count($viewData['aguardando']) ?></h3>
             </a>
         </div>
-    </div>
-    <div class="col-lg-3 col-md-6 col-sm-6 mb-3">
+    </a>
+</div>
+<div class="col-lg-3 col-md-6 col-sm-6 mb-3">
+    <a class="a">
         <div class="card text-white bg-dark o-hidden h-100">
             <div class="card-body">
                 <div class="mr-5">Encerrados</div>
@@ -38,7 +46,8 @@
                 <h3><?php echo count($viewData['encerrados']) ?></h3>
             </a>
         </div>
-    </div>
+    </a>
+</div>
 </div>
 
 <?php
